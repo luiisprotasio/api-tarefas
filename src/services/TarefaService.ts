7b10b481b8ec079c59f26b926ab604a6e6a4e95b
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-24 18:01:25 -0300

    feat: implementa os métodos CRUD

--- src/services/TarefaService.ts
+++ src/services/TarefaService.ts
@@ -8,16 +8,42 @@ done:boolean};
 interface CriarTarefa{
     name:string,
     desc:string};
-const bancoDeDados: Tarefa[] = [];
+let bancoDeDados: Tarefa[] = [];
 export class TarefaService {
     create({name,desc}:CriarTarefa){
         if (!name){
             throw new Error("Nome da tarefa é obrigatório");
         }
         const novaTarefa = {id:Math.floor(Math.random()*1000), title: name, description: desc, done: false};
+        bancoDeDados.push(novaTarefa);
         return novaTarefa;
     }
     list(){
         return bancoDeDados;
+    }
+    delete(idDelete:number){
+        const deletedTask = bancoDeDados.find((tarefa)=>tarefa.id === Number(idDelete));
+        if (!deletedTask){
+            throw new Error("Tarefa não encontrada");
+        }
+        bancoDeDados=bancoDeDados.filter((tarefa)=> tarefa !== deletedTask);
+
+    }
+     search(idSearch:number){
+        const searchedTask = bancoDeDados.find((tarefa)=>tarefa.id === Number(idSearch));
+        if (!searchedTask){
+            throw new Error("Tarefa não encontrada");
+        }
+        return searchedTask;
+    }
+    edit({name,desc}:CriarTarefa, taskId:number){
+         const editTask = bancoDeDados.find((tarefa)=>tarefa.id === Number(taskId));
+           
+        if (!editTask){
+            throw new Error("Tarefa não encontrada");
+        }
+        if (desc) {editTask.description=desc;}
+        if (name){editTask.title=name;}
+        return editTask;
     }
 }
