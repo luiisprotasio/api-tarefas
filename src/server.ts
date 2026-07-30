890f6c747ab37def33e815ccddc02b7aba1aeb86
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-30 02:01:37 -0300

    refactor: alinha o server.ts com o padrão iJunior

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-import express from 'express';
-import { tasksApp } from './routes/TarefaRoutes.js';
-const app = express();
-const PORT = 1412;
-app.use(express.json()); 
-app.use('/tasks', tasksApp);
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+import { app } from './config/expressConfig';
+
+app.listen(1412, () => {
+  console.log('🚀 Servidor rodando na porta 1412');
 });
